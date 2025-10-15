a9871d461131bab09070af1150f02c974e9595f1
dohed <desarrollo@coeurmkt.com>
2025-10-15 13:15:26 -0700

    changed font size on hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Hero() {
   return (
-    <section id="inicio" className="relative h-[500px] flex flex-col items-center justify-center overflow-hidden">
+    <section id="inicio" className="relative h-[600px] flex flex-col items-center justify-center overflow-hidden">
       <div className="absolute inset-0 z-0">
         <Image src="/assets/hero.jpg" alt="Green Garden Background" fill className="w-full h-full object-cover" />
         <div className="absolute inset-0 bg-gradient-to-r from-black/60 via-black/10 to-transparent"></div>
@@ -10,7 +10,7 @@ export default function Hero() {
 
       <div className="relative z-10 max-w-7xl mx-auto px-6 w-full">
         {/* Main text */}
-        <h1 className="text-4xl md:text-6xl lg:text-7xl font-light text-white leading-tight tracking-wide text-left">
+        <h1 className="text-4xl lg:text-6xl font-light text-white leading-tight tracking-wide text-left mt-10">
           Convertimos tu entorno
           <br />
           <span className="font-normal">en un paisaje</span>
